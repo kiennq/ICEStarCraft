9db53ba3d361cb0f50e5ed645f4f2435c73299c1
neik <icestarcraftbot@gmail.com>
2013-12-04 15:57:32 +0900

    change reserver value

--- ICEStarCraftModule/Addon Managers/BuildOrderManager.cpp
+++ ICEStarCraftModule/Addon Managers/BuildOrderManager.cpp
@@ -1076,8 +1076,8 @@ pair<int, BuildOrderManager::Resources> BuildOrderManager::reserveResources(Meta
 		t=nextFreeTime(builder,unitType);
 	pair<int, Resources> ret;
 	ret.first=t;
-	ret.second.minerals=unitType.mineralPrice();
-	ret.second.gas=unitType.gasPrice();
+	ret.second.minerals=unitType.mineralPrice()*2/3;
+	ret.second.gas=unitType.gasPrice()*2/3;
 	reserveResources(ret);
 	return ret;
 }
