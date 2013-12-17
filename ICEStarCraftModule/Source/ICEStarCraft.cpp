00e4d61803b6bc4cb7d88b3553ee3a385bcabc21
neik <icestarcraftbot@gmail.com>
2013-12-17 15:59:03 +0900

    fix line ending

--- ICEStarCraftModule/Source/ICEStarCraft.cpp
+++ ICEStarCraftModule/Source/ICEStarCraft.cpp
@@ -737,7 +737,11 @@ void ICEStarCraftModule::showDebugInfo()
 			Broodwar->drawTextMap(u->getPosition().x(),u->getPosition().y(),"%s",u->getOrder().getName().c_str());
       if (arbitrator.hasBid(u))
       {
-        Broodwar->drawTextMap(u->getPosition().x(),u->getPosition().y()-10,"%s %.2f", arbitrator.getHighestBidder(u).first->getName().c_str(), arbitrator.getHighestBidder(u).second);
+        pair<Arbitrator::Controller<Unit*, double>*, double> con = arbitrator.getHighestBidder(u);
+        Broodwar->drawTextMap(u->getPosition().x(),
+                              u->getPosition().y()-u->getType().dimensionUp()-10,
+                              "%s %.2f", con.first->getName().c_str(),
+                              con.second);
       }
 			
 			for each (Unit* e in Broodwar->enemy()->getUnits())
